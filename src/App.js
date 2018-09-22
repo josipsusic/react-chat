120021a5b958847e1cb62339c0404bcd6384835b
Josip Susic <josip.susic@hotmail.com>
2018-09-22 12:44:10 +0200

    Elements

---
+++ src/App.js
@@ -0,0 +1,24 @@
+//import React, { Component } from 'react';
+
+import React from 'react'
+import Chatkit from '@pusher/chatkit'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        );
+    }
+}
+
+export default App
