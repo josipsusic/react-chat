5b4f2fe2cfcb05d34c1b01854914b12fbd73f49e
Josip Susic <josip.susic@hotmail.com>
2018-09-25 20:09:20 +0200

    Created Message and turned into functional component

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,6 +4,8 @@ class Message extends React.Component {
     render() {
         return (
             <div className="message">
+            	<div className="message-username">{this.props.username}</div>
+                <div className="message-text">{this.props.text}</div>
             </div>
         )
     }
